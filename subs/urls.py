4ca98b1e456bb2385fdb1b0dfed70dd6d538f601
DylanMsK <kms920612@gmail.com>
2019-05-29 19:17:19 +0900

    delete test.html

--- subs/urls.py
+++ subs/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'subs'
 
 urlpatterns = [
-    path('', views.test, name='test'),
     path('<str:subUrl>/', views.redirect_url, name='redirect_url'),
 ]
--- subs/views.py
+++ subs/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def test(request):
-    return render(request, 'subs/test.html')
-
-
 def redirect_url(request, subUrl):
     context = {
-        'url_name': subUrl,
+        'subUrl': subUrl,
     }
-    return render(request, 'subs/index.html', context)
+    return render(request, 'subs/redirect_url.html', context)
