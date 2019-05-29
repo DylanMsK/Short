041560e1c4c4780100afce20ab68927260c13094
DylanMsK <kms920612@gmail.com>
2019-05-29 19:19:47 +0900

    migrate model

--- subs/models.py
+++ subs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Sub(models.Model):
+    origin = models.TextField()
+    new = models.TextField()
+
+    def __str__(self):
+        return f'{self.new}'
